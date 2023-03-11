05aac4ef59022f6ef8638ffc08185521feeeceb1
19alexandr80 <usanka1980@gmail.com>
2023-03-11 17:35:57 +0200

    ssd

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,8 +1,6 @@
 const buttonStart = document.querySelector('[data-start]');
 const buttonStop = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
-console.log(buttonStart);
-console.log(buttonStop);
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
@@ -12,7 +10,6 @@ function onBodyStyle() {
   weer = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
-  console.log('kkkkkkkkkkk');
   buttonStart.removeEventListener('click', onBodyStyle);
 }
 buttonStop.addEventListener('click', () => {
